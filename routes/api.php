032afa950a52be8112e726f343a5ac1e27eeef0a
diegocar448 <diegocar448@hotmail.com>
2018-10-29 14:41:03 -0300

    configurações iniciais 1

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,5 @@
 <?php
 
-/* $this->get('categories', 'Api\CategoryController@index');
-
-$this->post('categories', 'Api\CategoryController@store');
-
-$this->put('categories/{id}', 'Api\CategoryController@update');
-
-$this->delete('categories/{id}', 'Api\CategoryController@delete'); */
-
-/* $this->resources('categories', 'Api\CategoryController', [
-    'except' => ['edit', 'create']
-]); */
-
 
 //gerar o token
 $this->post('auth', 'Auth\AuthApiController@authenticate');
