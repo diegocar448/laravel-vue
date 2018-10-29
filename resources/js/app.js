dc0c9d6d36b68e5d8f544f9dea898b8b35ba451c
diegocar448 <diegocar448@hotmail.com>
2018-10-29 16:46:42 -0300

    Configurando VueRouter

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,13 +3,15 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import router from './routes/routers'
+
 /**
  * Componentes globais que podem ser usados
  * em qualquer lugar
  */
-
- Vue.component('test-component', require('./components/TestComponent.vue'))
+ Vue.component('test-component', require('./components/TestComponent'))
 
 const app = new Vue({
+    router,
     el: '#app'
 });
